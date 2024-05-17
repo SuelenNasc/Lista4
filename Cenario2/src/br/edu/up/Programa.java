6ac081e7f81f508eced78eab76636940d549c2f4
SuelenNasc <suh_nass@hotmail.com>
2024-05-17 17:52:28 -0300

    Cenario2inicio

--- Cenario2/src/br/edu/up/Programa.java
+++ Cenario2/src/br/edu/up/Programa.java
@@ -1,6 +1,26 @@
 package br.edu.up;
+
+import br.edu.up.modelos.Ponto;
+
 public class Programa {
     public static void main(String[] args) throws Exception {
-    
+
+        Ponto ponto1 = new Ponto();
+
+        Ponto ponto2 = new Ponto(2, 5);
+
+        double distanciaPonto1Ponto2 = ponto1.calcularDistancia(ponto2);
+        System.out.println("Distância do ponto1 ao ponto2: " + distanciaPonto1Ponto2);
+
+        double distanciaPonto2Coordenadas = ponto2.calcularDistancia(7, 2);
+        System.out.println("Distância do ponto2 às coordenadas (7,2): " + distanciaPonto2Coordenadas);
+
+        ponto1.setX(10);
+
+        ponto1.setY(3);
+
+        System.out.println("Novo valor de x no ponto1: " + ponto1.getX());
+        System.out.println("Novo valor de y no ponto1: " + ponto1.getY());
     }
 }
+
